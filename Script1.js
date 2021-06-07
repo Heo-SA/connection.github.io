b30ac4296e89986c29611b59c7387474c465c34c
Heotun <63722929+Heotun@users.noreply.github.com>
2021-06-07 22:38:26 +0900

    main complete
    메인 화면 기본 디자인 완료

---
+++ Script1.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+jQuery(document).ready(function ($) {
+
+	$('a.scroll-link').click(function (e) {
+		e.preventDefault();
+		$id = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $($id).offset().top - 20
+		}, 750);
+	});
+
+});
